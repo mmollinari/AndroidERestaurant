b5789d4bd2a30779c0de2a11be13c7906772f4fd
Marc MOLLINARI <marc.mollinari@it-ce.fr>
2020-12-30 11:20:40 +0100

    Fin partie II

--- app/src/main/java/fr/isen/mollinari/androiderestaurant/HomeActivity.kt
+++ app/src/main/java/fr/isen/mollinari/androiderestaurant/HomeActivity.kt
@@ -1,11 +1,51 @@
 package fr.isen.mollinari.androiderestaurant
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import androidx.annotation.StringRes
+import androidx.appcompat.app.AppCompatActivity
+import fr.isen.mollinari.androiderestaurant.databinding.ActivityHomeBinding
+
+private lateinit var binding: ActivityHomeBinding
 
 class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+
+        binding = ActivityHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.starters.setOnClickListener {
+            redirectToCategory(R.string.home_starters)
+        }
+
+        binding.mainDishes.setOnClickListener {
+            redirectToCategory(R.string.home_main_dishes)
+        }
+
+        binding.deserts.setOnClickListener {
+            redirectToCategory(R.string.home_desert)
+        }
+    }
+
+    private fun redirectToCategory(@StringRes categoryRes: Int) {
+        val intent = Intent(this, CategoryActivity::class.java)
+        intent.putExtra(CATEGORY_NAME, getString(categoryRes))
+        startActivity(intent)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        Log.d("HomeActivity", "Vous stoppez la page home")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d("HomeActivity", "Vous quittez la page home")
+    }
+
+    companion object {
+        const val CATEGORY_NAME = "CATEGORY_NAME"
     }
 }
